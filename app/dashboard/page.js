d8de091de71c4cf0a624bc4be04558f2c1ebd485
ved <ved20750327@gmail.com>
2025-09-15 10:46:08 +0530

    feat(dashboard): implement session check and redirect to login if not authenticated

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,6 +1,16 @@
-import React from 'react'
+"use client"
+import React, { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
+import { useSession,signOut } from 'next-auth/react'
 
 const Dashboard = () => {
+  const {data:session} = useSession()
+  const router = useRouter()
+  useEffect(() => {
+    if(!session){
+      router.push('/login')
+    }
+  }, [session,router])
   return (
     <div>
       Dashboard
