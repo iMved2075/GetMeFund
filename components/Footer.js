ce51ab1a6f7e22efa14aff4ce38790e02c1be5f4
ved <ved20750327@gmail.com>
2025-09-16 14:28:57 +0530

    fix(footer): correct company name formatting in copyright notice

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className='bg-gray-900 text-white h-13 flex items-center justify-center'>
-      <p className='text-center italic'>Copyright &copy; 2023 Get Me Fund. All rights reserved.</p>
+      <p className='text-center italic'>Copyright &copy; 2023 GetMeFund. All rights reserved.</p>
     </footer>
   )
 }
