be8694adcab38ae4fc4a8f6e70fd34343468b813
ved <ved20750327@gmail.com>
2025-09-21 15:13:21 +0530

    Footer: show current year dynamically

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
   return (
     <footer className='bg-gray-900 text-white h-13 flex items-center justify-center'>
-      <p className='text-center italic'>Copyright &copy; 2023 GetMeFund. All rights reserved.</p>
+      <p className='text-center italic'>Copyright &copy; {currentYear} GetMeFund. All rights reserved.</p>
     </footer>
   )
 }
