27a667327b43499e22520a25704ae951b42aa975
ved <ved20750327@gmail.com>
2025-09-21 15:13:19 +0530

    User profile page: validate user exists and set dynamic metadata

--- app/[username]/page.js
+++ app/[username]/page.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import { notFound } from 'next/navigation'
 import PaymentPage from '@/components/PaymentPage'
+import connectDb from '@/db/connectDb'
+import User from '@/models/User'
 
-const page = async({params}) => {
-  const {username} = await params
+const Username = async({ params }) => {
+  // Await params to get the actual object
+  const { username } = await params
+
+  const checkUser = async() => {
+    await connectDb()
+    let u = await User.findOne({ username: username })
+    if(!u) {
+      return notFound()
+    }
+  }
+  
+  await checkUser()
+  
   return (
     <>
       <PaymentPage username={username}/>
@@ -10,5 +25,12 @@ const page = async({params}) => {
   )
 }
 
-export default page
+export default Username
+
+export async function generateMetadata({ params }) {
+  const { username } = await params
+  return {
+    title: `Support ${username} - GetMeFund`,
+  }
+}
 
